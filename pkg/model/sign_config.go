e4de02b2cd47e738b9c25cdef44028d270e42137
wangbaotian <wangbaotian@jixiang.cn>
2025-02-25 17:14:44 +0800

    f

--- pkg/model/sign_config.go
+++ pkg/model/sign_config.go
@@ -1,17 +1,26 @@
 package model
 
-type SignConfigInterface interface {
-	SceneInterface
+import (
+	"context"
+
+	"gorm.io/gorm"
+)
+
+type SignConfigEntity struct {
+	// 签到配置id
+	Id int64
+	// 场景
+	Scene int64
 	// 签到类型 0中断后继续（不重置）， 1中断后重置
-	GetSignType() int64
+	SignType int64
 	// 签到单位，0每天，1每自然周，2每自然月
-	GetSignUnit() int64
+	SignUnit int64
 	// 有效期 开始时间戳，0表示不限
-	GetStartTs() int64
+	StartTs int64
 	// 有效期 结束时间戳，0表示不限
-	GetEndTs() int64
+	EndTs int64
 	// 签到奖励配置
-	GetSignRewardConfig() []SignRewardConfig
+	SignRewardConfig []SignRewardConfig
 }
 
 // 签到道具奖励
@@ -20,4 +29,11 @@ type SignRewardConfig struct {
 	PropEntity
 	// 道具发放数量
 	RewardPropNum int64
+}
+
+type SignConfigRepository interface {
+	// 根据场景获取签到配置
+	GetByScent(ctx context.Context, tx *gorm.DB, scene int64) *SignConfigEntity
+	// 创建签到配置
+	Create(ctx context.Context, tx *gorm.DB, signConfig *SignConfigEntity)
 }
--- pkg/model/user_sign.go
+++ pkg/model/user_sign.go
@@ -9,9 +9,9 @@ import (
 
 type UserSignTool interface {
 	// 用户签到
-	UserSign(ctx context.Context, user UserRepository, sign SignConfigInterface, ts time.Time) *UserSignResp
+	UserSign(ctx context.Context, user UserRepository, sign SignConfigEntity, ts time.Time) *UserSignResp
 	// 用户在当前时间，当前签到配置是否已经签到了
-	UserIsSigned(ctx context.Context, user UserRepository, sign SignConfigInterface, ts time.Time) bool
+	UserIsSigned(ctx context.Context, user UserRepository, sign SignConfigEntity, ts time.Time) bool
 }
 
 type UserSignInterface interface {
