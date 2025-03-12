9c1c99c1e2d67cc2402da805b0daef7422bec65b
wangbaotian <wangbaotian@jixiang.cn>
2025-03-12 14:01:12 +0800

    f

--- dist/cc/users/user.js
+++
@@ -1,9 +0,0 @@
-export class User {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    getUserInfo() {
-        return `Name: ${this.name}, Age: ${this.age}`;
-    }
-}
